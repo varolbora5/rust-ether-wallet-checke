c333cf968f6f8c949ae1134a893d81d27f41f020
Bora <varolbora5@gmail.com>
2023-01-23 21:44:48 +0100

    it is what it is

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,29 +1,9 @@
-use wagyu_model;
-use wallet_gen::prelude::*;
+pub fn make_wallet(value: u128) -> Result<wallet_gen::wallet::Wallet, dyn std::error::Error> {
+    let mut array:[u8; 32] = [0; 32];
+    let bytes = value.to_le_bytes();
 
-fn new_wallet(coin: Coin) -> Result<Wallet> {
-    let group = EcGroup::from_curve_name(Nid::SECP256K1)?;
-    let key = EcKey::generate(&group)?;
-    let pub_key = {
-        let mut bn_ctx = BigNumContext::new()?;
-        let mut vec =
-            key.public_key()
-                .to_bytes(&group, PointConversionForm::UNCOMPRESSED, &mut bn_ctx)?;
-        vec.remove(0);
-        vec
-    };
-    let priv_key = key.private_key().to_vec();
-    let hash_bytes = keccak256(&pub_key);
-
-    Ok(Wallet {
-        coin:        coin,
-        address:     format!("0x{:x}", &HexSlice::new(&hash_bytes[12..])),
-        public_key:  format!("{:x}", &HexSlice::new(&pub_key)),
-        private_key: format!("{:x}", &HexSlice::new(&priv_key[..])),
-        other:       None,
-    })
-}
-
-fn get_private_key(value: &[u8]) -> wagyu_model:: {
-
+    for i in 0..bytes.len() {
+        array[i] = bytes[i];
+    }
+    todo!()
 }
